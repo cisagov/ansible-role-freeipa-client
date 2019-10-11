cabfa9b6faccbd3783dae6823679f452eb5b7b7f
Felddy <markf@geekpad.com>
2019-10-11 16:56:54 -0400

    Work around systemd not playing will with uninstall.

--- files/setup_freeipa.sh
+++ files/setup_freeipa.sh
@@ -34,23 +34,60 @@ function get_ptr {
     dig +noall +ans -x "$1" | sed "s/.*PTR[[:space:]]*\(.*\)/\1/"
 }
 
-interface=$(get_interface)
-ip_address=$(get_ip "$interface")
+function enroll {
+    # Check to see if freeipa-client is already installed
+    if [[ -f "/etc/ipa/default.conf" ]]; then
+        echo "FreeIPA client is already installed... rejoining."
+        echo ${ADMIN_PW} | kinit admin@${REALM}
+        ipa-join || true
+        kdestroy
+        exit 0
+    fi
 
-# Wait until the IP address has a non-Amazon PTR record before
-# proceeding
-ptr=$(get_ptr "$ip_address")
-while grep amazon <<< "$ptr"
-do
-    sleep 30
+    interface=$(get_interface)
+    ip_address=$(get_ip "$interface")
+
+    # Wait until the IP address has a non-Amazon PTR record before
+    # proceeding
     ptr=$(get_ptr "$ip_address")
-done
+    while grep amazon <<< "$ptr"
+    do
+        sleep 30
+        ptr=$(get_ptr "$ip_address")
+    done
 
-ipa-client-install --realm="${REALM}" \
-                   --principal=admin \
-                   --password="${ADMIN_PW}" \
-                   --mkhomedir \
-                   --hostname="${HOSTNAME}" \
-                   --no-ntp \
-                   --unattended \
-                   --force-join
+    ipa-client-install --realm="${REALM}" \
+                       --principal=admin \
+                       --password="${ADMIN_PW}" \
+                       --mkhomedir \
+                       --hostname="${HOSTNAME}" \
+                       --no-ntp \
+                       --unattended \
+                       --force-join
+}
+
+function unenroll {
+  echo ${ADMIN_PW} | kinit admin@${REALM}
+  ipa-join --unenroll
+  rm /etc/krb5.keytab
+  kdestroy
+  exit 0
+}
+
+if [ $# -lt 1 ]
+then
+    echo "command required: enroll | unenroll"
+    exit 255
+fi
+
+case "$1" in
+    enroll)
+      enroll
+      ;;
+    unenroll)
+      unenroll
+      ;;
+    *)
+    echo "unknown command.  Valid commands are: enroll | unenroll"
+    exit 255
+esac
