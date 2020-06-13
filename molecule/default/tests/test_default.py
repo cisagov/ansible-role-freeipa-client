4f72f882975eb44927d1b51867e51fd96548549f
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-06-13 12:45:55 -0400

    Remove systemd jazz from tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -27,23 +27,12 @@ def test_packages_fedora(host, pkg):
 
 
 @pytest.mark.parametrize(
-    "f,mode,content",
-    [
-        ("/usr/local/sbin/setup_freeipa.sh", 0o500, ""),
-        ("/lib/systemd/system/freeipa-enroll.service", 0o644, "freeipa-enroll"),
-    ],
+    "f,mode", [("/usr/local/sbin/setup_freeipa.sh", 0o500)],
 )
-def test_files(host, f, mode, content):
+def test_files(host, f, mode):
     """Test that the appropriate files were installed."""
     assert host.file(f).exists
     assert host.file(f).is_file
     assert host.file(f).user == "root"
     assert host.file(f).group == "root"
     assert host.file(f).mode == mode
-    assert host.file(f).contains(content)
-
-
-@pytest.mark.parametrize("service", ["freeipa-enroll"])
-def test_services(host, service):
-    """Test that the expected services were enabled."""
-    assert host.service(service).is_enabled
