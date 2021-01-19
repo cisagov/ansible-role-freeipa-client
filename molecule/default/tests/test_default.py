f18242c650b30f63b35c3a1aac97a8ff511432a2
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2021-01-19 15:49:37 -0500

    Add installation of sssd-tools
    This package is being added because we occassionally need sss_cache and related tools when debugging sssd-related issues on FreeIPA clients.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,14 +12,14 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-@pytest.mark.parametrize("pkg", ["ipa-client"])
+@pytest.mark.parametrize("pkg", ["ipa-client", "sssd-tools"])
 def test_packages_amazon(host, pkg):
     """Test that the appropriate packages were installed."""
     if host.system_info.distribution == "amzn":
         assert host.package(pkg).is_installed
 
 
-@pytest.mark.parametrize("pkg", ["freeipa-client"])
+@pytest.mark.parametrize("pkg", ["freeipa-client", "sssd-tools"])
 def test_packages_not_amazon(host, pkg):
     """Test that the appropriate packages were installed."""
     if host.system_info.distribution != "amzn":
